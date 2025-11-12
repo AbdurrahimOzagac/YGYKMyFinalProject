b6512deb25ba25f55eaecc9f8418576ad7ece320
AbdurrahimOzagac <abdozagac@gmail.com>
2025-11-12 10:55:39 +0300

    Create 4-layer architecture (Entities, DataAccess, Business, ConsoleUI) and add basic startup code

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using Entities.Concrete;
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
